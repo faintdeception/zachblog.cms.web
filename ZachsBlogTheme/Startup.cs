f6fab2d6c7cddce82ecf17021f2242f4f307311e
Zach Ruffin <faintdeception@gmail.com>
2022-01-23 15:53:38 -0500

    Upgraing to .net 6.0 and orchard core 1.2

--- ZachsBlogTheme/Startup.cs
+++ ZachsBlogTheme/Startup.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using OrchardCore.Modules;
 using OrchardCore.ResourceManagement;
 
@@ -8,7 +9,9 @@ namespace OrchardCore.Themes.ZachsBlogTheme
     {
         public override void ConfigureServices(IServiceCollection serviceCollection)
         {
-            serviceCollection.AddScoped<IResourceManifestProvider, ResourceManifest>();
+            //serviceCollection.AddScoped<IResourceManifestProvider, ResourceManifest>();
+
+            serviceCollection.AddTransient<IConfigureOptions<ResourceManagementOptions>, ResourceManagementOptionsConfiguration>();
         }
     }
 
